bcc49945e547944570d74b92df67ceecd3df834a
adamstrv <adam@oink-oink-oink.net>
2024-12-11 11:03:23 +0100

    replace .xlsx opener with .csv opener

--- extract_descriptor_types.py
+++ extract_descriptor_types.py
@@ -1,1 +1,6 @@
 # Extract descriptor types
+
+def extract_descriptor_types(SMILES_strings):
+    """
+    Function which extract ALL descriptor types from an input SMILES strings and returns these.
+    """
