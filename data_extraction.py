818a27eb099919b07a77dda69a599f16781f920f
Hormann <b.hormann@student.tue.nl>
2024-12-18 09:17:11 +0100

    improved data extraction

--- data_extraction.py
+++ data_extraction.py
@@ -1,17 +1,12 @@
 # Function which extracts data from given .csv files
 
-def extract_train_data(train_csv_path):
+def extract_train_smiles(train_csv_path):
     """
-    Function which extract the train and test data from an input .csv file. It extracts column data
-    and creates a dictionary in which data is catagorized based on the characteristic if a SMILES string 
-    can or can't bind to the dopamine receptor DRD3 for the training data. It only extract values from the 
-    columns for the test data.
-    Output: The function returns four things, a list of SMILES strings for the test set, a dictionary with all
-    values for the training data, a separate dictionary for only strings that CAN bind, and a separate dictionary 
-    of the strings that CAN'T bind with the DRD3 dopamine receptor.
+    Function that extract the smiles with their classification from a .csv file
+    it returns a dictonary with a smile strings and their classifications
     """
 
-    train_file = open(train_csv_path)    # Open and read file
+    train_file = open(train_csv_path)                       # Open and read file
     train_lines = train_file.readlines()
     train_file.close()
 
@@ -22,25 +17,25 @@ def extract_train_data(train_csv_path):
         smiles = values[0].strip('"')                       # The SMILES string is the first element of the list made of split line
         binary_classification = int(values[1].strip('"'))   # Classification is the second element
         train_smiles[smiles] = binary_classification        # Add to dictionary
-    
- #   test_smiles = []                                        # Test data:  make empty list for test data
-   
 
     return train_smiles
 
-r = extract_train_data('train.csv')
 
+def extract_test_smiles(test_csv_path):
+    """
+    Function that extract the smiles with their classification from a .csv file
+    it returns a list with a smile strings
+    """
 
-#test_file = open(test_csv_path)
+    test_file = open(test_csv_path)                         # Open and read file
+    test_lines = test_file.readlines()
+    test_file.close()
 
-# Make list of training values
-#test_lines = test_file.readlines()
+    test_smiles = []                                        # Make empty list for train SMILES
     
-# Iterate thourgh test lines: Skip the first line (header)   
-#for line in test_lines[1:]:  
-#    # The SMILES string is the second element of the list made of split line
-#    smiles = line.strip().split(',')[1]
-#    cleaned_smiles = smiles.strip('"')
-#    test_smiles.append(cleaned_smiles)
+    for line in test_lines[1:]:                             # Iterate thourgh test lines: Skip the first line (header) 
+        smiles = line.strip().split(',')[1]
+        cleaned_smiles = smiles.strip('"')
+        test_smiles.append(cleaned_smiles)
 
-#test_smiles = []  
+    return test_smiles
--- extract_descriptor_types.py
+++ extract_descriptor_types.py
@@ -6,7 +6,7 @@ import pandas as pd
 #rdkit.Chem.Descriptors.CalcMolDescriptors -> dictionary descriptor: value
 
 # Import function which extracts data from .csv files
-from data_extraction import extract_data
+from data_extraction import extract_train_smiles
 
 def extract_descriptor_types(smiles_strings):
     """
@@ -17,43 +17,35 @@ def extract_descriptor_types(smiles_strings):
     Returns an excel sheet of the dataframe containing the values of the descriptor types for all molecules. 
     !!! The index represents the 'Unique_ID' of the SMILES strings !!!
     """
-
-    # Make empty list for descriptors
-    descriptors = []
-
-    # Make case for test set: it is just a list input
-    if isinstance(smiles_strings, list):
-        for smiles in smiles_strings:
-            # Convert SMILES string into RDKit molecule object
-            mol = Chem.MolFromSmiles(smiles)
-            descriptor = Descriptors.CalcMolDescriptors(mol) 
-            # Add to descriptor list, includes dictionary for each molecule
+    descriptors = []                                                 # Make empty list for descriptors
+    
+    if isinstance(smiles_strings, list):                             # Make case for test set: it is just a list input
+        for smile in smiles_strings:
+            mol = Chem.MolFromSmiles(smile)                         # Convert SMILES string into RDKit molecule object
+            descriptor = Descriptors.CalcMolDescriptors(mol)         # Add to descriptor list, includes dictionary for each molecule
             descriptors.append(descriptor)
+        
+        data_frame = pd.DataFrame(descriptors)  
 
-    # Make case for training set: dictionary input
-    elif isinstance(smiles_strings, dict):
-        # Iterate over SMILES in dictionary and make list of descriptor types
-        for smiles in smiles_strings.keys():
-            # Convert SMILES string into RDKit molecule object
-            mol = Chem.MolFromSmiles(smiles)
-            descriptor = Descriptors.CalcMolDescriptors(mol)
-            # Add to descriptor list, includes dictionary for each molecule
+    elif isinstance(smiles_strings, dict):                           # Make case for training set: dictionary input
+        for smiles in smiles_strings.keys():                         # Iterate over SMILES in dictionary and make list of descriptor types
+            mol = Chem.MolFromSmiles(smiles)                         # Convert SMILES string into RDKit molecule object
+            descriptor = Descriptors.CalcMolDescriptors(mol)         # Add to descriptor list, includes dictionary for each molecule
             descriptors.append(descriptor)
+        
+        data_frame = pd.DataFrame(descriptors) 
+        data_frame['classification'] = list(smiles_strings.values())
     
     else:
         raise TypeError("Input has to be either a list or dictionary (with the keys being the SMILES string).")
     
     # Create dataframe to return results
-    df = pd.DataFrame(descriptors)
-    
-    # Adjust index to start from 1 since index represents 'Unique_ID'
-    df.index = pd.RangeIndex(start = 1, stop = len(df) + 1)
-    
-    # Return results in excel file
-    df.to_excel('Descriptor_analysis.xlsx', sheet_name='descriptor_results', index=True)
-    return descriptors
+
+    # df.index = pd.RangeIndex(start = 1, stop = len(df) + 1)                                  # Adjust index to start from 1 since index represents 'Unique_ID'
+    # df.to_excel('Descriptor_analysis.xlsx', sheet_name='descriptor_results', index=True)     # Return results in excel file
+    return data_frame
 
 
-
-test_smiles = extract_data('test.csv', 'train.csv')[0]
-extract_descriptor_types(test_smiles)
+train_smiles= extract_train_smiles('shortertrain.csv')
+predictors = extract_descriptor_types(train_smiles)
+print(predictors)
