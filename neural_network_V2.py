bca40bd118d3d8b36c914c1868bba6c26acea419
Hormann <b.hormann@student.tue.nl>
2025-01-04 15:00:59 +0100

    please run Tuning

--- neural_network_V2.py
+++ neural_network_V2.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 # this is just a modded model from a website, i'll merge it toghether with my old model later
 class NN_BinClass(nn.Module):
-    def __init__(self, input_size = 80 , n_layers=3, n_neurons = 40, neuron_reduction = 1, dropout_rate = 0.3, learning_rate = 0.005, momentum = 0.95):
+    def __init__(self, activation = nn.ReLU6, input_size = 80 , n_layers=3, n_neurons = 40, neuron_reduction = 1, dropout_rate = 0.3, learning_rate = 0.005, momentum = 0.95, ):
         super().__init__()
         self.layers = []
         self.acts = []
@@ -20,10 +20,11 @@ class NN_BinClass(nn.Module):
             self.add_module(f"act{i}", self.acts[-1])
             input_size = n_neurons
             n_neurons = round(neuron_reduction * n_neurons)
-        self.output = nn.Linear(n_neurons, 1)
+        self.linear_out = nn.Linear(n_neurons, 1)
+        self.sigmoid = nn.Sigmoid()
     
         
-        self.dropout = nn.Dropout(dropout)
+        self.dropout = nn.Dropout(dropout_rate)
 
         self.lr = learning_rate
         self.momentum = momentum
@@ -33,8 +34,9 @@ class NN_BinClass(nn.Module):
     def forward(self, x):
         for layer, act in zip(self.layers, self.acts):
             x = act(layer(x))
-        x = self.output(x)
-        return x
+        x = self.linear_out(x)
+        out = self.sigmoid(x)
+        return out
 
 
     def train_one_opoch(self, loader):
